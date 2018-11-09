9f833e30c1396f2f7bc177e3c9fe6af94865b41f
mewturn <pathonice@hotmail.com>
2018-11-09 15:51:09 +0800

    Add pip installation

--- flaskapp/requirements.sh
+++ flaskapp/requirements.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Installing Python requirements for warren
-# Requirements: Python 3.x
+# Requirements: Python 3.x, Pip
+
+echo "Installing pip..."
+
+apt-get install python3-pip
+
+echo "Finished installing pip!"
 
 echo "Installing requirements for warren..."
 
@@ -8,6 +14,7 @@ echo "Installing requirements for warren..."
 pip3 install flask
 pip3 install jieba
 pip3 install mysql-connector-python
+pip3 install virtualenv
 pip3 install OpenCC
 
-echo "Completed!"
+echo "Finished installing requirements!"

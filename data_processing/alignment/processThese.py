c741c225ed1193884d7d42e88e3502dbdfcfd446
mewturn <pathonice@hotmail.com>
2018-12-24 16:03:05 +0800

    Initial commit

---
+++ data_processing/alignment/processThese.py
@@ -0,0 +1,14 @@
+import sys
+
+if __name__ == "__main__":
+    files_to_process = sys.argv[1]
+    output = sys.argv[2]
+
+    with open(files_to_process, "r", encoding="utf-8") as inp:
+        with open(output, "w", encoding="utf-8") as outp:
+            for line in inp:
+                en_file, zh_file = line.split(" | ")
+                en_file = en_file.replace("\n", "")
+                zh_file = zh_file.replace("\n", "")
+                outp.write("python smush.py %s processed_eng/%s en\n" % (en_file, en_file.replace("EN_txt/", "")))
+                outp.write("python smush.py %s processed_chi/%s zh\n" % (zh_file, zh_file.replace("CHI_txt/", "")))

ec4f2cb2a42f2e47a7d5e4cb11e409a1ed5205d1
mewturn <pathonice@hotmail.com>
2018-12-25 14:42:42 +0800

    Add output for LFAligner

--- data_processing/alignment/processThese.py
+++ data_processing/alignment/processThese.py
@@ -10,5 +10,10 @@ if __name__ == "__main__":
                 en_file, zh_file = line.split(" | ")
                 en_file = en_file.replace("\n", "")
                 zh_file = zh_file.replace("\n", "")
-                outp.write("python smush.py %s processed_eng/%s en\n" % (en_file, en_file.replace("EN_txt/", "")))
-                outp.write("python smush.py %s processed_chi/%s zh\n" % (zh_file, zh_file.replace("CHI_txt/", "")))
+                
+                # Write smush batch
+                # outp.write("python smush.py %s processed_eng/%s en\n" % (en_file, en_file.replace("EN_txt/", "")))
+                # outp.write("python smush.py %s processed_chi/%s zh\n" % (zh_file, zh_file.replace("CHI_txt/", "")))
+
+                # Write LFAligner batch
+                outp.write("LF_aligner_4.1.exe -f=t -l=en,zh -s=y -r=xn -t=n -i=%s,%s" % (en_file.replace("EN_txt/", "processed_eng/"), zh_file.replace("CHI_txt/", "processed_chi/")))
